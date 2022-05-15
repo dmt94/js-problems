804e14eedf5c590536972bf4c344419b899323e7
Daevah Taylaran <daevtayl@gmail.com>
2022-05-14 18:59:13 -0700

    add

--- js-problems/keyless-car.js
+++ js-problems/keyless-car.js
@@ -1,16 +1,33 @@
 // Make a keyless car EVEN BETTER!
 // We are improving our car from previous exercise now.
 // Solutions to future exercises will be in the .js file (meaning Javascript file) from now on.
+let rlSync = require('readline-sync');
 
-var age = prompt("What is your age?");
+let age = rlSync.question("What is your age?\n");
 
-if (Number(age) < 18) {
-	alert("Sorry, you are too yound to drive this car. Powering off");
-} else if (Number(age) > 18) {
-	alert("Powering On. Enjoy the ride!");
-} else if (Number(age) === 18) {
-	alert("Congratulations on your first year of driving. Enjoy the ride!");
+let checkDriverAge2 = function () {
+  if (Number(age) < 18) {
+    console.log("Sorry, you are too yound to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    console.log("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    console.log("Congratulations on your first year of driving. Enjoy the ride!");
+  }
 }
+
+function checkDriverAge(age) {
+  if (Number(age) < 18) {
+    console.log("Sorry, you are too young to drive this car. Powering off");
+  } else if (Number(age) > 18) {
+    console.log("Powering On. Enjoy the ride!");
+  } else if (Number(age) === 18) {
+    console.log("Congratulations on your first year of driving. Enjoy the ride!");
+  }
+}
+
+checkDriverAge2();
+console.log(checkDriverAge(20));
+
 
 //1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
 // Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
@@ -18,5 +35,5 @@ if (Number(age) < 18) {
 //2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
 
 //BONUS: Instead of using the prompt. Now, only use console.log and make the checkDriverAge() function accept an argument of age, so that if you enter:
-checkDriverAge(92);
-it returns "Powering On. Enjoy the ride!"
+
+// it returns "Powering On. Enjoy the ride!"
